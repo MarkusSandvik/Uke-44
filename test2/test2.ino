e55721f6980f70501f6d18cf6d79c67cabfe017e
MarkusSandvik <markus.sandvik@hotmail.com>
2023-11-02 15:27:37 +0100

    Integration and tests made at end of day in test2

--- test2/test2.ino
+++ test2/test2.ino
@@ -17,11 +17,14 @@ int8_t timesCharged = 0;
 // Variables for hiddenFeature()
 bool hiddenActivated = false;
 bool emergencyChargingUsed = false;
+bool emergencyChargeMode = false;
 
 // Variables for showBatteryStatus()
 unsigned long previousMillis = 0;
-const long oninterval = 10000;
-const long offinterval = 2000;
+unsigned long refreshPreviousMillis = 0;
+const long onInterval = 10000;
+const long offInterval = 2000;
+const long refreshInterval = 500;
 long displayTime = 0;
 bool batteryDisplayed = false;
 
@@ -29,47 +32,73 @@ bool batteryDisplayed = false;
 int16_t previousCountLeft = 0;
 int16_t previousCountRight = 0;
 
+///////// TEST VARIABLES ////
+long distance = 100;
+int batteryHealth = 2;
+float iAmSpeed = 0;
+
 
 
 void setup(){
-
+    Serial.begin(9600);
+    // Wait for button A to be pressed and released.
+    display.clear();
+    display.print(F("Press A"));
+    display.gotoXY(0, 1);
+    display.print(F("to start"));
+    buttonA.waitForButton();
+    
 }
 
 void loop(){
-
+    softwareBattery();
+    showBatteryStatus();
+    if (buttonA.isPressed())
+    {
+        motors.setSpeeds(200, 200);
+    }
+    else if (buttonC.isPressed())
+    {
+        motors.setSpeeds(-400, -400);
+    }
+    else
+    {
+        motors.setSpeeds(0, 0);
+    }
 }
 
-int speedometer(){
+float speedometer(){
     static uint8_t lastDisplayTime;
-    static uint8_t OneSecond = 1000;
 
     long countsLeft = encoders.getCountsLeft();
     long countsRight = encoders.getCountsRight();
     if ((uint8_t)(millis() - lastDisplayTime) >= 100)
-  {
-    int16_t newCountLeft = countsLeft - previousCountLeft;
-    int16_t newCountRight = countsRight - previousCountRight;
-    int16_t avrage = (newCountLeft+newCountRight)/2;
-    float distanse = 75.81*12;
-    float oneRound = 12.25221135;
-    float meters = avrage/distanse*oneRound;
-    Serial.println(meters);
-    previousCountLeft = countsLeft;
-    previousCountRight = countsRight;
-    //display.clear();
-    //display.print(meters);
-    lastDisplayTime = millis();
-    return meters;
-}
+    {
+        int16_t newCountLeft = countsLeft - previousCountLeft;
+        int16_t newCountRight = countsRight - previousCountRight;
+        int16_t avrage = (newCountLeft+newCountRight)/2;
+        float distanse = 75.81*12;
+        float oneRound = 122.5221135;
+        float meters = avrage/distanse*oneRound;
+        //Serial.println(meters);
+        previousCountLeft = countsLeft;
+        previousCountRight = countsRight;
+        //display.clear();
+        //display.print(meters);
+        lastDisplayTime = millis();
+        iAmSpeed = meters;
+        return meters;
+    } // end if
+} // end void
 
 
 void softwareBattery(){
-    long distance = distancemeter();
-    int8_t averageSpeed = speedometer();
+    float averageSpeed = speedometer();
+    //Serial.print(averageSpeed);
     int8_t distanceChange = distance - lastDistance;
     lastDistance = distance;
 
-    consumptionMeasure += (averageSpeed / distanceChange) // EKSEMPEL PÅ FUNKSJON, OPPDATER NÅR VI TESTER MED DATA
+    consumptionMeasure += (averageSpeed / distanceChange); // EKSEMPEL PÅ FUNKSJON, OPPDATER NÅR VI TESTER MED DATA
 
     if (consumptionMeasure >= 10){
         batteryLevel -= 1;
@@ -87,7 +116,6 @@ void carNeedCharging(){
 } // end void
 
 void hiddenFeature(){
-    long distance = distancemeter();
     int8_t averageSpeed = speedometer();
     int8_t distanceChange = distance - lastDistance;
 
@@ -105,7 +133,7 @@ void hiddenFeature(){
       
         lastDistance = distance;
 
-        consumptionMeasure += (averageSpeed / distanceChange) // EKSEMPEL PÅ FUNKSJON, OPPDATER NÅR VI TESTER MED DATA
+        consumptionMeasure += (averageSpeed / distanceChange); // EKSEMPEL PÅ FUNKSJON, OPPDATER NÅR VI TESTER MED DATA
 
         if (consumptionMeasure <= -10){
             if ((emergencyChargeMode == true) && (emergencyChargingUsed = false)){
@@ -122,11 +150,31 @@ void hiddenFeature(){
 
 void showBatteryStatus(){                               
     unsigned long currentMillis = millis();
+    
 
     ///////// DISABLE WHILE CHARGING //////////
+    if (batteryDisplayed == false){
+        if (currentMillis - refreshPreviousMillis >= refreshInterval){
+            //float speedReading = speedometer();
+            display.clear();
+            display.setLayout11x4();                        // Divide screen into 11 columns and 4 rows
+            display.print(F("Speed:"));
+            display.gotoXY(0,1);
+            display.print(iAmSpeed);
+            display.gotoXY(7,1);
+            display.print(F("m/s"));
+            display.gotoXY(0,2);
+            display.print(F("Distance:"));
+            display.gotoXY(0,3);
+            display.print(distance);
+            display.gotoXY(7,3);
+            display.print(F("m"));
+            refreshPreviousMillis = currentMillis;
+        } // end if
+    } // end if
 
 
-    if (currentMillis - previousMillis >= oninterval){
+    if (currentMillis - previousMillis >= onInterval){
         display.clear();
         display.setLayout21x8();                        // Divide screen into 21 columns and 8 rows
         display.print(F("Battery level"));
@@ -145,20 +193,7 @@ void showBatteryStatus(){
         batteryDisplayed = true;                        // To make the next if sentence only run once after this text have been ran
     } // end if
 
-    if ((currentMillis - displayTime >= offinterval) && (batteryDisplayed == true)){
-        display.clear();
-        display.setLayout11x4();                        // Divide screen into 11 columns and 4 rows
-        display.print(F("Speed:"));
-        display.gotoXY(0,1);
-        display.print(speedReading);
-        display.gotoXY(7,1);
-        display.print(F("m/s"));
-        display.gotoXY(0,2);
-        display.print(F("Distance:"));
-        display.gotoXY(0,3);
-        display.print(distance);
-        display.gotoXY(7,3);
-        display.print(F("m"));
+    if ((currentMillis - displayTime >= offInterval) && (batteryDisplayed == true)){
         batteryDisplayed = false;
     } // end if
 } // end void
